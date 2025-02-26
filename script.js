9d355a7a21f2216ee589a022cdaaa56085e8f44b
saurabh-patil-9787 <154727373+saurabh-patil-9787@users.noreply.github.com>
2025-02-26 12:26:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+const student = {
+	name:"saurabh",
+	age:29,
+	city:"HU",
+	Address:"nf12222"
+}
+
+function getKeys(obj) {
+	return Object.keys(obj)
+}
+
+console.log(getKeys(student))
