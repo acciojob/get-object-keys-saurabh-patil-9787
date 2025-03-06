9bb5e7838b2838c0992528596e3150603d2fe275
saurabh-patil-9787 <154727373+saurabh-patil-9787@users.noreply.github.com>
2025-03-06 11:37:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
+// Create the student object with a name property
 const student = {
-	name:"saurabh",
-	age:29,
-	city:"HU",
-	getKeys: function() {
-		return Object.keys(this);
-	}
-}
+    name: "John Doe"
+};
 
-const keys = student.getKeys()
-console.log(keys)
+// Another student object with multiple properties
+const student2 = {
+    name: "Alice",
+    age: 25,
+    city: "New York"
+};
+
+// Function to get the keys of an object
+const displayKeys = document.getElementById('keys')
+function getKeys(obj) {
+    return Object.keys(obj);
+}
+displayKeys.innerText = getKeys(student) +" " + getKeys(student2)
+// Testing the function
+// console.log(getKeys(student));  // Output: ["name"]
+// console.log(getKeys(student2)); // Output: ["name", "age", "city"]
