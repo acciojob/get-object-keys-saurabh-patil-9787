003a4551c5ba0d2b74e622e120cd81b7d7ec3628
saurabh-patil-9787 <154727373+saurabh-patil-9787@users.noreply.github.com>
2025-03-06 11:40:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ const displayKeys = document.getElementById('keys')
 function getKeys(obj) {
     return Object.keys(obj);
 }
-displayKeys.innerText = getKeys(student) +" " + getKeys(student2)
+// displayKeys.innerText = getKeys(student) +" " + getKeys(student2)
 // Testing the function
-// console.log(getKeys(student));  // Output: ["name"]
-// console.log(getKeys(student2)); // Output: ["name", "age", "city"]
+console.log(getKeys(student));  // Output: ["name"]
+console.log(getKeys(student2)); // Output: ["name", "age", "city"]
